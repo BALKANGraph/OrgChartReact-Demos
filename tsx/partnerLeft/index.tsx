4396b9dc1c65094692edb12d087eae6c4014c086
Zornitsa Serbezova <zornitsa.pesheva@outlook.com>
2026-05-20 09:20:39 +0300

    partners left and children

---
+++ tsx/partnerLeft/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { OrgChartReact } from "balkan-orgchart-react";
+
+function ChartExample() {
+  return (
+    <OrgChartReact
+      nodeBinding={{ field_0: "id" }}
+      style={{ width: "100%", height: "100%" }}
+      enableSearch={false}
+      data={[
+        { id: 1 },
+        { id: 2, pid: 1, tags: ['left-partner'] },
+        { id: 3, pid: 1 },
+        { id: 4, pid: 1 }
+      ]}
+    >
+
+    </OrgChartReact>
+  );
+}
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChartExample />
+  </React.StrictMode>
+);
