0afd282fa715675b18b923834b3216debc317282
balkangraph <zornitsa.pesheva@outlook.com>
2026-05-21 17:01:58 +0300

    pdf preview

---
+++ tsx/exportPdfPreview/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { OrgChartReact } from "balkan-orgchart-react";
+
+export const ChartExample = () => {
+  return  <OrgChartReact 
+            nodeBinding={{ field_0: 'name', field_1: 'title' }}
+            menu={{ pdf_preview: { text: "PDF Preview" } }}
+            nodeMenu={{ pdf_preview: { text: "PDF Preview" } }}
+            enableSearch={false}
+            data={[
+              { id: 1, name: "Billy Moore", title: "CEO" },
+              { id: 2, pid: 1, name: "Billie Rose", title: "Dev Team Lead" },
+              { id: 3, pid: 1, name: "Glenn Bell", title: "HR" },
+              { id: 4, pid: 3, name: "Blair Francis", title: "HR" }
+            ]}
+          >
+           
+          </OrgChartReact>
+}
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChartExample />
+  </React.StrictMode>
+);
