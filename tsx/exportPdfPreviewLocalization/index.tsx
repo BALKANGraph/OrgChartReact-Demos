d5cae34266f88d46466cf094f2d83d3efaeb20b6
Zornitsa Serbezova <zornitsa.pesheva@outlook.com>
2026-05-22 09:23:31 +0300

    export pdf examples

---
+++ tsx/exportPdfPreviewLocalization/index.tsx
@@ -0,0 +1,59 @@
+import React, { useRef } from "react";
+import { createRoot } from "react-dom/client";
+import { OrgChartReact, OrgChartJS } from "balkan-orgchart-react";
+
+export const ChartExample = () => {
+
+  const chartRef = useRef<OrgChartJS>(null);
+
+  function pdfPreview(){
+    if (chartRef.current) {
+        chartRef.current.pdfPreviewUI.locCancel = '关闭';
+        chartRef.current.pdfPreviewUI.locAddNew = '添加新页面';
+        chartRef.current.pdfPreviewUI.locExport = '导出';
+        chartRef.current.pdfPreviewUI.locRemove = '删除页面';
+        chartRef.current.pdfPreviewUI.locLandscape = '横向';
+        chartRef.current.pdfPreviewUI.locPortrait = '纵向';
+        chartRef.current.pdfPreviewUI.locClickToAdd = '点击一个节点以在此处显示';
+
+        chartRef.current.pdfPreviewUI.show({
+          format: "A4",
+          footer: `<text text-anchor="end" style="font-size: 24px;">
+                    第 {current-page} 页，共 {total-pages} 页
+                  </text>`
+        });
+    }
+  }
+
+  const handleInit = () => {
+    pdfPreview();
+  }
+
+  return  <OrgChartReact 
+            ref={chartRef}  
+            nodeBinding={{ field_0: 'name', field_1: 'title' }}
+            enableSearch={false}
+            onInit={handleInit}
+            menu={{
+              previewPDF: {
+                  text: "导出 PDF", 
+                  icon: OrgChartJS.icon.pdf(24, 24, "#7A7A7A"), 
+                  onClick: pdfPreview
+              }
+            }}
+            data={[
+              { id: 1, name: "张伟", title: "首席执行官" },
+              { id: 2, pid: 1, name: "王昊", title: "开发团队负责人" },
+              { id: 3, pid: 1, name: "李静", title: "人力资源" },
+              { id: 4, pid: 3, name: "刘欣", title: "人力资源" }
+            ]}
+          >
+           
+          </OrgChartReact>
+}
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChartExample />
+  </React.StrictMode>
+);
