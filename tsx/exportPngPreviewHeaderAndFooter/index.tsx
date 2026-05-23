6f6bb1f921ede77d0c5ec87bdafa99a7aff27080
balkangraph <zornitsa.pesheva@outlook.com>
2026-05-23 11:06:08 +0300

    png preview headeer and footer

---
+++ tsx/exportPngPreviewHeaderAndFooter/index.tsx
@@ -0,0 +1,43 @@
+import React, { useRef } from "react";
+import { createRoot } from "react-dom/client";
+import { OrgChartReact, OrgChartJS } from "balkan-orgchart-react";
+
+
+export const ChartExample = () => {
+  
+  const chartRef = useRef<OrgChartJS>(null);
+  
+  function pngPreview() {
+    chartRef.current?.pngPreviewUI.show({
+      header: '<text>My Header</text>',
+      footer: '<text>My Footer. Page {current-page} of {total-pages}</text>'
+    });
+  }
+  
+  return  <OrgChartReact 
+            ref={chartRef}
+            nodeBinding={{ field_0: 'name', field_1: 'title' }}
+            menu={{ 
+              pngPreview: {
+                text: "PNG Preview with Header and Footer",
+                icon: OrgChartJS.icon.png(24, 24, "#7A7A7A"),
+                onClick: pngPreview
+              } 
+          }}
+            enableSearch={false}
+            data={[
+              { id: 1, name: "Billy Moore", title: "CEO" },
+              { id: 2, pid: 1, name: "Billie Rose", title: "Dev Team Lead" },
+              { id: 3, pid: 1, name: "Glenn Bell", title: "HR" },
+              { id: 4, pid: 3, name: "Blair Francis", title: "HR" }
+            ]}
+          >
+           
+          </OrgChartReact>
+}
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChartExample />
+  </React.StrictMode>
+);
