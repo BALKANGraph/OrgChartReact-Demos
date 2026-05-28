6b2d31fd9bdae63fa6ef4c7af5b20cd660f7c032
Zornitsa Serbezova <zornitsa.pesheva@outlook.com>
2026-05-28 06:12:28 +0300

    export event listeners

---
+++ tsx/exportEventListeners/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { OrgChartReact } from "balkan-orgchart-react";
+
+export const ChartExample = () => {
+  return  <OrgChartReact 
+            nodeBinding={{ field_0: 'name', field_1: 'title' }}
+            menu={{ 
+              csv_export: { text: "Export CSV" },
+              xml_export: { text: "Export XML" },
+            }}
+            nodeMenu={{ csv_export: { text: "Export CSV" } }}
+            enableSearch={false}
+            data={[
+                { id: 1, name: "Billy Moore", title: "CEO", img: "https://cdn.balkan.app/shared/2.jpg" },
+                { id: 2, pid: 1, name: "Billie Rose", title: "Dev Team Lead", img: "https://cdn.balkan.app/shared/5.jpg" },
+                { id: 3, pid: 1, name: "Glenn Bell", title: "HR", img: "https://cdn.balkan.app/shared/10.jpg" },
+                { id: 4, pid: 3, name: "Blair Francis", title: "HR", img: "https://cdn.balkan.app/shared/11.jpg" }
+            ]}
+          >
+          </OrgChartReact>
+}
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChartExample />
+  </React.StrictMode>
+);
