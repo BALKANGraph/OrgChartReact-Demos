709c6856411421acaf202d03ecb05bb84aa8cce2
Zornitsa Serbezova <zornitsa.pesheva@outlook.com>
2026-05-28 07:46:10 +0300

    import issue

--- tsx/Import/index.tsx
+++ tsx/Import/index.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
 import { createRoot } from "react-dom/client";
 import { OrgChartReact, OrgChartJS } from "balkan-orgchart-react";
 
 export const ChartExample = () => {
+
+  const chartRef = useRef<OrgChartJS>(null);
+
   return  <OrgChartReact 
             nodeBinding={{ field_0: 'name', field_1: 'title' }}
             menu={{ 
               xml_export: { text: "Export XML" },
               csv_export: { text: "Export CSV" },
               json_export: { text: "Export JSON" },
-              jsonImport: { text: "Import JSON", icon: OrgChartJS.icon.json(24, 24, 'red'), onClick: importJSONHandler },
-              xmlImport: { text: "Import XML", icon: OrgChartJS.icon.xml(24, 24, 'red'), onClick: importXMLHandler },
-              csvImport: { text: "Import CSV", icon: OrgChartJS.icon.csv(24, 24, 'red'), onClick: importCSVHandler }
+              jsonImport: { text: "Import JSON", icon: OrgChartJS.icon.json(24, 24, 'red'), onClick: () => chartRef.current?.importJSON() },
+              xmlImport: { text: "Import XML", icon: OrgChartJS.icon.xml(24, 24, 'red'), onClick: () => chartRef.current?.importXML() },
+              csvImport: { text: "Import CSV", icon: OrgChartJS.icon.csv(24, 24, 'red'), onClick: () => chartRef.current?.importCSV() }
              }}
             enableSearch={false}
             data={[
