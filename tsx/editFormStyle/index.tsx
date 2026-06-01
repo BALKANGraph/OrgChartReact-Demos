bf82c94dbbe0d3be0d02b606b6c2f02c0e19d7dc
ZornitsaPesheva <pesheva@gmail.com>
2026-06-01 12:44:53 +0300

    edit form style

---
+++ tsx/editFormStyle/index.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useRef } from "react";
+import { createRoot } from "react-dom/client";
+import { OrgChartReact } from "balkan-orgchart-react";
+
+export const ChartExample = () => {
+
+
+  return  <OrgChartReact 
+            layout={OrgChartJS.layout.mixed}
+            data={[
+              { id: 1 },
+              { id: 2, pid: 1, tags: ['yellow'] },
+              { id: 3, pid: 1, tags: ['orange'] }
+            ]}  
+            nodeBinding={{
+              field_0: "EmployeeName",
+              field_1: "Title",
+              img_0: "ImgUrl"
+            }}>
+          </OrgChartReact>
+}
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChartExample />
+  </React.StrictMode>
+);
