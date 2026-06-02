e55e857c5117e4dcbc2236bd740bec7983c7039f
balkangraph <zornitsa.pesheva@outlook.com>
2026-06-02 16:13:56 +0300

    undo redo

--- tsx/undoRedoDatabaseUpdate/index.tsx
+++ tsx/undoRedoDatabaseUpdate/index.tsx
@@ -5,15 +5,19 @@ import { OrgChartReact } from "balkan-orgchart-react";
 function ChartExample() {
   return (
       <OrgChartReact
+        undoRedoStorageName="undoRedoStorage"
         nodeBinding={{ field_0: "name", field_1: "title", img_0: "img" }}
         style={{ width: "100%", height: "100%" }}
-        mode="dark"
-        template="olivia"
         data={[
           { id: 1, name: "Amber McKenzie", title: "CEO", img: "https://cdn.balkan.app/shared/empty-img-none.svg" },
           { id: 2, pid: 1, name: "Ava Field", title: "IT Manager", img: "https://cdn.balkan.app/shared/empty-img-none.svg" },
           { id: 3, pid: 1, name: "Rhys Harper", img: "https://cdn.balkan.app/shared/empty-img-none.svg" }
         ]}
+        nodeMenu={{
+          add: {text: 'Add'},
+          edit: {text: 'Edit'},
+          remove: {text: 'Remove'},
+        }}
       >
       </OrgChartReact>
   );
