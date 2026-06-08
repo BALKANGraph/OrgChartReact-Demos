2869a15e85669716704d9023f2245cd675aea1e5
Zornitsa Serbezova <zornitsa.serbezova@live.com>
2026-06-08 08:48:19 +0300

    orientation

---
+++ tsx/orientationTop/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { OrgChartReact, OrgChartJS } from "balkan-orgchart-react";
+
+function ChartExample() {
+  return (
+      <OrgChartReact
+        orientation={OrgChartJS.orientation.top}
+        data={[
+          { id: 1 },
+          { id: 2, pid: 1 },
+          { id: 3, pid: 1 }
+        ]}
+      >
+      </OrgChartReact>
+  );
+}
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChartExample />
+  </React.StrictMode>
+);
